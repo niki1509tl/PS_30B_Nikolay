654fbaa45c80133c576aa843b11735c9d2037cee
Niki <nl@auxcode.com>
2022-04-21 15:10:45 +0300

    initial set for userpresenter

---
+++ StudentInfoSystem/ViewModel/UserPresenter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StudentInfoSystem.Model;
+using System.Windows.Input;
+
+namespace StudentInfoSystem.ViewModel
+{
+    class UserPresenter : ObservableObject
+    {
+        private User _user;
+
+        public User User
+        {
+            get
+            {
+                return _user;
+            }
+            set
+            {
+                _user = value;
+                RaisePropertyChangedEvent("User");
+            }
+        }
+
+        public ICommand LoginCommand
+        {
+            get { return new DelegateCommand(LoginValidation); }
+        }
+        public void LoginValidation()
+        {
+
+        }
+    }
+}
